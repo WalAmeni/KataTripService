0742a5d95e91cde1baa0127ff73d891487a3b4a4
Ameni WALHA <ameni@ahmeds-macbook-pro.home>
2018-10-31 17:50:52 +0100

    refactoring TripService

--- src/main/java/net/dammak/kata/tripservice/trip/TripService.java
+++ src/main/java/net/dammak/kata/tripservice/trip/TripService.java
@@ -11,31 +11,45 @@ import java.util.List;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedInUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = tripsByUser(user);
-			}
-			return tripList;
-		} else {
+		//User loggedUser = getLoggedInUser();
+		
+		if (getLoggedInUser() == null) {
 			throw new UserNotLoggedInException();
 		}
-	}
+		//	List<Trip> tripList = new ArrayList<Trip>();
 
-	protected List<Trip> tripsByUser(User user) {
-		return TripDAO.findTripsByUser(user);
-	}
+		//if (loggedUser != null) {
+		//boolean isFriend = false;
+		//for (User friend : user.getFriends()) {
+		//	if (friend.equals(loggedUser)) {
+		//		isFriend = true;
+		//		break;
+		//	}
+		//}
+		//
+		//if (user.isFriendWith(loggedUser)) {
+		//	return tripsByUser(user);
+		//}else {
+		//return new ArrayList<Trip>() ;
 
-	protected User getLoggedInUser() {
-		return UserSession.getInstance().getLoggedUser();
-	}
+		return user.isFriendWith(getLoggedInUser())
+				? tripsByUser(user)
+				: noTrips() ;
 	
+}
+
+
+	private ArrayList<Trip> noTrips() {
+		return new ArrayList<Trip>();
+	}
+
+
+protected List<Trip> tripsByUser(User user) {
+	return TripDAO.findTripsByUser(user);
+}
+
+protected User getLoggedInUser() {
+	return UserSession.getInstance().getLoggedUser();
+}
+
 }
